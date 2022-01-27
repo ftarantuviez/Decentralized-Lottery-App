5e3303c5e06871c1679b2b86d259cd2f8f03bd4e
Francisco Tarantuviez <tarantuviezf@gmail.com>
2022-01-27 10:20:43 -0300

    feat(production): build optimized bundle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import lottery from "./lottery";
+import web3 from "./web3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  state = { manager: "", players: [], balance: "", value: "", message: "" };
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!window.ethereum) {
+      return alert("Install Metamask please!");
+    }
+    await window.ethereum.enable();
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: "Waiting on transaction success..." });
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether"),
+    });
+
+    this.setState({ message: "You have been entered!" });
+  };
+
+  pickAWinner = async (event) => {
+    if (!window.ethereum) {
+      return alert("Install Metamask please!");
+    }
+    await window.ethereum.enable();
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: "Waiting on transaction success..." });
+
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+
+    this.setState({ message: "A winner has been picked!" });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This contract is managed by {this.state.manager} <br /> There are
+          currently {this.state.players.length} competing to win{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <hr />
 
+        <form onSubmit={this.handleSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              onChange={(event) => this.setState({ value: event.target.value })}
+              value={this.state.value}
+            />
+          </div>
+          <button type="submit">Enter</button>
+        </form>
+        <hr />
+        <h4>Ready to pick a winner</h4>
+
+        <button onClick={this.pickAWinner}>Pick a winner</button>
+        <hr />
+
+        <p>{this.state.message}</p>
+      </div>
+    );
+  }
+}
 export default App;
